69aebb8418074b7442bbe9925dcc0cc118d91fda
Isabel Corry <s2587604@ed.ac.uk>
2024-07-04 14:56:00 +0100

    Resampled Mean Geotiff Files

--- Resampled_Mean.R
+++ Resampled_Mean.R
@@ -12,7 +12,7 @@ main <- function() {
   +     input_file_ESA <- "ESA_Extract_Merged.tif"
   +     input_file_ICESat2 <- "ICESat2_Cornwall_1B.tif"
   +     output_file_ESA <- "ESA_Resampled_Mean.tif"
-  +     outout_file_ICESat2 <- "ICESat2_Resampled_Mean.tif"
+  +     output_file_ICESat2 <- "ICESat2_Resampled_Mean.tif"
   +     new_width <- 800
   +     new_height <- 1300 
   +     Resample_Geotiff(input_file_ESA, output_file_ESA, new_width, new_height)
