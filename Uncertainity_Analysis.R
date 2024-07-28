6b4d82d776c26ffbea04c2885757ac338b388a3c
Isabel Corry <s2587604@ed.ac.uk>
2024-07-28 16:06:46 +0100

    Code used for ucnertainity analysis

---
+++ Uncertainity_Analysis.R
@@ -0,0 +1,34 @@
+setwd('P:/Geotiff')
+library(terra)
+library(dplyr)
+library(ggplot2)
+calculate_90_ci <- function(raster_data) {
+  +     values <- values(raster_data, na.rm = TRUE)
+  +     lower_bound <- quantile(values, probs = 0.05, na.rm = TRUE)
+  +     upper_bound <- quantile(values, probs = 0.95, na.rm = TRUE)
+  +     return(c(lower_bound, upper_bound))
+  + }
+GEDI <- rast("GEDI_Cornwall.tif")
+ESA <- rast("ESA_Resample_Mean.tif")
+ICESat2 <- rast("ICESat2_Resample_Mean.tif")
+ALS <- rast("ALS_Biomass_Extract_Resampled.tif")
+GEDI_ci <- calculate_90_ci(GEDI)
+ESA_ci <- calculate_90_ci(ESA)
+ICESat2_ci <- calculate_90_ci(ICESat2)
+ALS_ci <- calculate_90_ci(ALS)
+print(paste("GEDI 90% CI", GEDI_ci[1], "-", GEDI_ci[2]))
+print(paste("ESA 90% CI:", ESA_ci[1], "-", ESA_ci[2]))
+print(paste("ICESat2 90% CI:", ICESat2_ci[1], "-", ICESat2_ci[2]))
+print(paste("ALS 90% CI:", ALS_ci[1], "-", ALS_ci[2]))
+ci_df <- data.frame(
+  +     Dataset = c("GEDI", "ESA", "ICESat2", "ALS"), 
+  +     Lower_Bound = c(GEDI_ci[1], ESA_ci[1], ICESat2_ci[1], ALS_ci[1]),
+  +     upper_Bound = c(GEDI_ci[2], ESA_ci[2], ICESat2_ci[2], ALS_ci[2])
+  +     )
+print(ci_df)
+ci_df$Midpoint <- (ci_df$Lower_Bound + ci_df$upper_Bound) / 2
+ggplot(ci_df, aes(x = Dataset, y = Midpoint)) +
+  +     geom_bar(stat = 'identity', fill = 'skyblue') + 
+  +     geom_errorbar(aes(ymin = Lower_Bound, ymax = upper_Bound), width = 0.2) + 
+  +     labs(title = "90th Percentile Confidence Intervals of AGBD", x = "Dataset", y = "Biomass Density") +
+  +     theme_minimal()
